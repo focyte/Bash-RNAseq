90ebd2aaf8ecd642588d1e3e1bee75ace5191480
John Foster <focyte@gmail.com>
2022-12-25 20:56:42 +0000

    Create FastqcTrimMultipleFiles.sh

---
+++ FastqcTrimMultipleFiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# install the required tools
+sudo apt-get -y install hisat2
+sudo apt-get -y install samtools
+sudo apt intall trim-galore
+pip install cutadapt
+sudo apt install fastqc
+curl -fsSL https://github.com/FelixKrueger/TrimGalore/archive/0.6.6.tar.gz -o trim_galore.tar.gz
+tar xvzf trim_galore.tar.gz
+sudo apt-get install rna-star
+
+# navigate to the folder
+cd ./Documents/Human_RNASeq
+
+# run fastqc on all files
+find . -maxdepth 1 -name \*fastq.gz -exec fastqc {} \;
+
+# list files to check that it has made the correct files
+ls -ltr
+
+# This did not work, therefore ran without --fastqc
+find . -maxdepth 1 -name \*fastq.gz -exec trim_galore {} \;
+
+#then run the report again
+find . -maxdepth 1 -name \*trimmed_fastq.gz -exec fastqc {} \;
