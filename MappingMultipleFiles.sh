c1211e3544ee13778585e0e55afc0fd5022efd84
John Foster <focyte@gmail.com>
2023-08-03 21:38:38 +0100

    Update MappingMultipleFiles.sh

--- MappingMultipleFiles.sh
+++ MappingMultipleFiles.sh
@@ -1,3 +1,3 @@
 for file in /home/john/Documents/project2/*.fastq; do
-    hisat2 [options] -x /home/john/Documents/project2/Index -p 8 -U "$file" -S "${file%.fastq}.sam" --known-splicesite-infile /path/to/splicing_annotation.gtf
+    hisat2 [options] -x /home/john/Documents/project2/Index -p 8 -U "$file" -S "${file%.fastq}.sam" --known-splicesite-infile /home/john/Documents/project2/Index/human_splice_sites.txt
 done
