7ac869bf325eba4dd955afa36a5adbf6a96b6f37
john <focyte@gmail.com>
2023-08-19 18:51:09 +0100

    Replaced old code with new bash script

--- MappingMultipleFiles.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for file in ./*.fastq; do
-    hisat2 [options] -x ./Index/ -p 8 -U "$file" -S "${file%.fastq}.sam" --known-splicesite-infile ./Index/human_splice_sites.txt
-done
--- count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt install subread
-
-featureCounts -T 4 -s 2 \
-  -a yeast_gene_models.gtf \
-  -o featurecounts.txt \
-  SRR453566_yeast_rnaseq_trimmed_sorted.bam
