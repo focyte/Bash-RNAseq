f87c3035e480e72c885d5f74231a6cb3cbb20a5c
John Foster <focyte@gmail.com>
2023-12-24 22:57:42 +0000

    Added QC scripts to new combined pipeline folder

---
+++ Combined/trim_fastq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check for the correct number of arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input_dir> <output_dir>"
+    exit 1
+fi
+
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
+
+# Loop through each fastq.gz file in the input directory
+for fastq_file in "$INPUT_DIR"/*.fastq.gz; do
+        trim_galore "$fastq_file" -o "$OUTPUT_DIR"
+
+    if [ $? -eq 0 ]; then
+        echo "Trimmed $fastq_file"
+    else
+        echo "Error trimming $fastq_file"
+    fi
+done
